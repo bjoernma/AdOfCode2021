8a58a322bd6b846168ac892faea68b3d797a33e2
Björn M <bjoern.mahrt@gmx.de>
2021-12-02 20:06:04 +0100

    Modify python file to use the aim and solve second part of day 2

--- 02/adventofcode_2021-02.py
+++ 02/adventofcode_2021-02.py
@@ -4,21 +4,27 @@ input_file=open("adventofcode_2021-02_input.txt","r")
 
 posh=0
 depth=0
+aim=0
 
 for line in input_file:
     actual_line=line.split(" ")
     if actual_line[0]=="forward":
         posh+=int(actual_line[1])
+        depth+=int(actual_line[1])*aim
     if actual_line[0]=="backward":
         posh-=int(actual_line[1])
     if actual_line[0]=="down":
-        depth+=int(actual_line[1])
+#        depth+=int(actual_line[1])
+        aim+=int(actual_line[1])
     if actual_line[0]=="up":
-        depth-=int(actual_line[1])
+#        depth-=int(actual_line[1])
+        aim-=int(actual_line[1])
+#    print("actual_line: ",actual_line[1]," new posh: ",posh," new depth: ",depth," new aim: ",aim)
 input_file.close()
 
 print("posh: ",posh)
 print("depth: ",depth)
+print("aim: ",aim)
 
 print("Product: posh*depth = ",posh*depth)
 
